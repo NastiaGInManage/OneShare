26da61c6caa6e375495d8eac20a088340191d613
Nastia Gusev <nastiag@inmanage.net>
2023-06-11 14:23:41 +0300

    Initial Commit

---
+++ OneShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneShare
+//
+//  Created by NastiaGusev on 11/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
