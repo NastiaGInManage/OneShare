79dccbeb84999eb70623f953e06264cb21c761aa
Nastia Gusev <nastiag@inmanage.net>
2023-06-11 16:12:59 +0300

    added function for sharing gif

--- OneShare/ViewController.swift
+++ OneShare/ViewController.swift
@@ -13,7 +13,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-//
+    
+    @IBAction func didTapShare(_ sender: Any) {
+        sendGif(strUrl: "https://cmsone.inmanage.com/_media/media/49/741.gif?t=1672730905")
+    }
+    
+    func sendGif(strUrl: String) {
+        guard let fileURL = URL(string: strUrl) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: fileURL) { (data, response, error) in
+            guard let gifData = data else {
+                if let error = error {
+                    print("Failed to download GIF data: \(error)")
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let activityViewController = UIActivityViewController(activityItems: [gifData], applicationActivities: nil)
+                activityViewController.setValue(self.title, forKey: "subject")
+                self.present(activityViewController, animated: true, completion: nil)
+            }
+        }
+        task.resume()
+    }
 
 }
 
